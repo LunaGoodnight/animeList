5db5204e5a4faa2bbacd27fc87e2dc5e3bbbe6fc
Luna Goodnight <luna.lee@nextfortune.tw>
2022-12-15 18:04:59 +0800

    [Add] init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import styled from "styled-components";
+import Abyss from "./images/abyss.webp";
 
+const AnimeListWrapper = styled.ul`
+  display: flex;
+  width: 100%;
+  list-style: none;
+`;
+
+const AnimeItem = styled.li`
+  border: ${(props) =>
+    props.selected ? "2px solid darkcyan" : "2px solid gray"};
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+`;
+
+const ImageBox = styled.div`
+  width: 20rem;
+  height: 20rem;
+`;
+const animeCollections = [
+  { title: "來自深淵", images: Abyss },
+  { title: "美少女戰士" },
+];
+
+const PageBody = styled.div`
+  width: 100%;
+`;
 function App() {
+  const [animeList, setAnimeList] = useState([]);
+  const hasSelected = (title) => {
+    console.log({ test: "wowo" });
+    return animeList.find((item) => item.title === title);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>測試你的肥宅指數！</h2>
+        <p>選擇你看過的動畫</p>
+        <PageBody>
+          <AnimeListWrapper>
+            {animeCollections.map((x, i) => {
+              const { title, images } = x;
+              return (
+                <AnimeItem
+                  selected={hasSelected(title)}
+                  key={i}
+                  onClick={() =>
+                    hasSelected(title)
+                      ? setAnimeList((prev) =>
+                          prev.filter((it) => it.title !== title)
+                        )
+                      : setAnimeList((prev) => prev.concat(x))
+                  }
+                >
+                  <h4>{title}</h4>
+                  <ImageBox>
+                    <img src={images} alt={title} />
+                  </ImageBox>
+                </AnimeItem>
+              );
+            })}
+          </AnimeListWrapper>
+        </PageBody>
       </header>
     </div>
   );
