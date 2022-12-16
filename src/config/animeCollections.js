7425953d33d10d19a769cfb026193bdb334dc62b
Luna Goodnight <luna.lee@nextfortune.tw>
2022-12-16 15:26:16 +0800

    [Add] new anime

--- src/config/animeCollections.js
+++ src/config/animeCollections.js
@@ -1,9 +1,11 @@
 import Abyss from "../images/abyss.webp";
 import ZeroKara from "../images/zero.webp";
 import FullMetal from "../images/fullmetal.webp";
+import SAO from "../images/sao.webp";
 
 export const animeCollections = [
   { title: "來自深淵", images: Abyss },
   { title: "從零開始的異世界生活", images: ZeroKara },
   { title: "鋼之鍊金術師", images: FullMetal },
+  { title: "刀劍神域", images: SAO },
 ];
