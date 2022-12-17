924034627f4a9bdd7c78edbdb44c8cfa1d851acc
Luna Goodnight <luna.lee@nextfortune.tw>
2022-12-17 19:06:59 +0800

    [Add] new anime

--- src/config/animeCollections.js
+++ src/config/animeCollections.js
@@ -12,6 +12,9 @@ import Nana from "../images/nana.webp";
 import Book from "../images/book.webp";
 import Home from "../images/home.webp";
 import Kano from "../images/kano.webp";
+import Ichigo from "../images/ichigo.webp";
+import GoSick from "../images/gosick.webp";
+import BookShelf from "../images/bookshelf.webp";
 
 export const animeCollections = [
   { title: "來自深淵", images: Abyss },
@@ -22,10 +25,13 @@ export const animeCollections = [
   { title: "約定的夢幻島", images: Dream },
   { title: "花牌情緣", images: Chihayafuru },
   { title: "獵人", images: Hunter },
-  { title: "家有女友", images: Kano },
-  { title: "出租女友", images: Hunter },
+  { title: "家有女友", images: Home },
+  { title: "出租女友", images: Kano },
   { title: "機械女神", images: Lime },
   { title: "九龍珠", images: Nine },
   { title: "NANA", images: Nana },
   { title: "小書痴的下剋上", images: Book },
+  { title: "草莓100%", images: Ichigo },
+  { title: "GO SICK", images: GoSick },
+  { title: "丹特麗安的書架", images: BookShelf },
 ];
